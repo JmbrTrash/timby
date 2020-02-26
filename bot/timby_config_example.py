c5d7ba234baaa01643d744797b9bd15a0a7826ec
Dean Verjans <developer.dean@outlook.com>
2020-02-26 13:20:22 +0100

    fixed tuple issue in example config

--- bot/timby_config_example.py
+++ bot/timby_config_example.py
@@ -2,7 +2,7 @@
 API_TOKEN='enter token here'
 
 #Database settings
-MYSQL_HOST='localhost',
-MYSQL_DATABASE='timby',
-MYSQL_USER='root',
-MYSQL_PASSWORD='root',
+MYSQL_HOST='localhost'
+MYSQL_DATABASE='timby'
+MYSQL_USER='root'
+MYSQL_PASSWORD='root'
